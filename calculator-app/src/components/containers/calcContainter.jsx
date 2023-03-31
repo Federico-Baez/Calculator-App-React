e55d9fab497589be5c7cf08484ba183236fc9a04
Federico-Baez <baezfede95@gmail.com>
2023-03-30 21:13:22 -0300

    feat: responsive design

--- calculator-app/src/components/containers/calcContainter.jsx
+++ calculator-app/src/components/containers/calcContainter.jsx
@@ -39,9 +39,9 @@ const CalcContainter = ({ handleThemeChange, theme }) => {
     const divide = (num1, num2) => {
         if (typeof num1 === "number" && typeof num2 === "number") {
             if (num2 != 0) {
-                return setScreenText(((num1 / num2) * 100) / 100);
+                return setScreenText(Math.round((num1 / num2) * 100) / 100);
             } else {
-                setScreenText("Math ERROR 1");
+                setScreenText("Math ERROR");
                 setErrorFlag(true);
             }
         } else {
@@ -108,7 +108,7 @@ const CalcContainter = ({ handleThemeChange, theme }) => {
         }
         //error message and flag used when the lenght of screen text >15
         else if (screenText.length > 15) {
-            setScreenText("Math ERROR 2");
+            setScreenText("Math ERROR");
             setErrorFlag(true);
 
         } else {
